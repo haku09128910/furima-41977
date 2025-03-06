7a3d46683097fdc6bf253a1a1e5ab511fe83b2c4
haku09128910 <h0912-8910@au.com>
2025-03-06 20:01:49 +0900

    Reapply "テスト完了"
    This reverts commit 7c594fa39177281c524d51852c65d8f717989543.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+<<<<<<< Updated upstream
 
   validates :nickname, presence: true
 
@@ -27,3 +28,6 @@ class User < ApplicationRecord
 
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合で入力してください' }
 end
+=======
+end
+>>>>>>> Stashed changes
