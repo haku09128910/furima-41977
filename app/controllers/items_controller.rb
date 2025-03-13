8fe9ba77f2395cdb53ba3e603d69788ba8a59768
haku09128910 <h0912-8910@au.com>
2025-03-13 12:23:28 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,9 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    # トップページの処理
+
     @items = Item.all
+    # binding.pry
   end
 
   def new
@@ -24,4 +25,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :price, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_day_id, :image).merge(user_id: current_user.id)
   end
+
 end
