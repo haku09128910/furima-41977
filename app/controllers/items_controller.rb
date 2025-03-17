6944efb84d8e6bcc8ccef72af0a7f2423d37b2a4
haku09128910 <h0912-8910@au.com>
2025-03-17 18:29:38 +0900

    編集機能実装3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :edit,]
-  before_action :move_to_index, except: [:index, ]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update]
   def index
     @items = Item.order(created_at: :desc)
