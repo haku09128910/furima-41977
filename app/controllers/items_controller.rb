44e190af71f692331a3a7f40bdfd3a618956d6b2
haku09128910 <h0912-8910@au.com>
2025-03-17 17:35:21 +0900

    編集機能実装2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show,]
-
+  before_action :authenticate_user!, except: [:index, :show, :edit,]
+  before_action :move_to_index, except: [:index, ]
+  before_action :move_to_index, only: [:edit, :update]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -40,6 +41,13 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :description, :price, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_day_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user == current_user
+      redirect_to action: :index
+    end
   end
 
 end
