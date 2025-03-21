dac304661e097cb9e76d99d55cf95708b77f3ebd
haku09128910 <h0912-8910@au.com>
2025-03-21 14:12:52 +0900

    購入機能修正7

--- spec/factories/shipping_addresses.rb
+++ spec/factories/shipping_addresses.rb
@@ -1,11 +1,4 @@
-
+FactoryBot.define do
   factory :shipping_address do
-    order
-    postal_code { '123-4567' }
-    prefecture_id { 2 }
-    city { '東京都' }
-    address { '1-1-1' }
-    building { '' }
-    phone_number { '09012345678' }
   end
-
+end
